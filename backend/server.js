38d42268fa4ec08929b1cefdf2c62c0c1d7e8740
Obrad Lacman <lacmansoftware@gmail.com>
2022-06-16 20:30:20 +0900

    add signup and saving user feature in backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,35 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "./.env" });
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Successful response.');
-});
+const corsOptions = {
+  origin: "http://192.168.108.87:3000",
+};
 
-app.listen(5000, () => console.log('Example app is listening on port 5000.'));
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const authRouter = require("./routes/auth");
+
+app.get("/", (req, res) => {
+  res.send("Successful response.");
+});
+app.use("/auth", authRouter);
+
+/* Starting Server */
+app.listen(5000, () => console.log("Example app is listening on port 5000."));
+
+/* Connecting DB */
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log("mongodb connected successfully");
+  })
+  .catch((error) => {
+    console.error("mongodb connection failed");
+  });
